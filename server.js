709f07f7a400727e2a232dfaf451a539c6f5d537
Geovanio Carnelosso <geovanio@atualcs.com.br>
2019-08-13 07:59:43 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import expressGraphQL from 'express-graphql';
+import cors from 'cors';
+import mongoose from 'mongoose';
+require('dotenv').config();
+const app = express();
+app.use(
+  cors(),
+  bodyParser.json()
+)
+app.use(
+  "/graphql",
+  expressGraphQL({
+  schema: {},
+  rootValue: {},
+  graphiql: true
+  })
+);
+function main() {
+  const port = process.env.PORT || 5000;
+  const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@geoaep-gimoh.mongodb.net/vaporcenter?retryWrites=true&w=majority/`;
+  mongoose.connect(uri, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+main();
