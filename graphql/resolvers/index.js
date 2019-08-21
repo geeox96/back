ff88a7785ed8731967663fb3c2654d22dd9edd10
geeox96 <geeox96@gmail.com>
2019-08-21 13:01:56 -0300

    Update index.js

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,6 +1,6 @@
 const Query = require('./Query')
 const Mutation = require('./Mutation')
-const Fabricante = require('./type/Fabricante')
+const Fabricante = require('./Type/Fabricante')
 
 module.exports = {
     Query,
